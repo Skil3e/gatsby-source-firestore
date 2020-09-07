c01322d31e9a5147cd28c21815e4005439746ae9
Skil3e <manosm.design@gmail.com>
2020-09-07 16:09:07 +0300

    Initial commit

--- README.md
+++ README.md
@@ -3,28 +3,14 @@ Gatsby source plugin for Firestore
 
 ## Usage
 1. ```npm install @Skil3e/gatsby-source-firestore```
-2. use .env.* or hardcode your login credentials and firebase config
+2. Generate credentials from Firebase > Settings > Service accounts > "Generate new private key".
 3. Configure settings at ```gatsby-config.js```, for example:
 ```js
 module.exports = {
     {
         resolve: `skil3e-source-firestore`,
         options: {
-            login: {
-                //your login credentials
-                email: process.env.FIREBASE_AUTH_EMAIL,
-                password: process.env.FIREBASE_AUTH_PASSWORD
-            },
-            firebaseConfig: {
-                apiKey: process.env.FIREBASE_API_KEY,
-                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-                databaseURL: process.env.FIREBASE_DATABASE_URL,
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-                appId: process.env.FIREBASE_APP_ID,
-                measurementId: process.env.FIREBASE_MEASUREMENT_ID
-            },
+            credential: require(`./credentials.json`),
             collections: [
                 {
                     type: "Collection", // Graphql name
@@ -46,22 +32,7 @@ module.exports = {
     }
 }
 ```
-if you use ```makeNode: false``` in ```subCollections```
-```graphql
-{
-  allCollection {
-    edges {
-      node {
-        value
-        childrenSubcollection  {
-          value
-        }
-      }
-    }
-  }
-}
-```
-if you use ```makeNode: true``` in ```subCollections```
+
 ```graphql
 {
   allCollection {
@@ -75,8 +46,8 @@ if you use ```makeNode: true``` in ```subCollections```
     }
   }
 
-  //will also make this
-  allSubcollection{
+  """ Will also make this """
+  allSubcollection {
     edges {
       node {
         value 
@@ -84,3 +55,9 @@ if you use ```makeNode: true``` in ```subCollections```
     }
   }
 }
+
+## Acknowledgement
+
+- [ryanflorence/gatsby-source-firebase](https://github.com/ryanflorence/gatsby-source-firebase)
+- [taessina/gatsby-source-firestore](https://github.com/taessina/gatsby-source-firestore)
+
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,81 +1,67 @@
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/firestore");
-
+const firebase = require('firebase-admin');
 exports.sourceNodes = async ({actions, createNodeId, createContentDigest}, configOptions) => {
     const {createNode} = actions;
     delete configOptions.plugins;
     //Config variables
-    const {collections, firebaseConfig, login} = configOptions;
-
+    const {collections, credential} = configOptions;
     //Firebase initialize
-    const firebaseApp = firebase.initializeApp(firebaseConfig);
-    const db = firebaseApp.firestore();
-    const auth = firebaseApp.auth();
-
-    auth.signInWithEmailAndPassword(login.email, login.password).then(() => {
-        //Iterate Collections
-        collections.forEach(({type, name, map = node => node, subCollections}) => {
-            //Get Collection
-            db.collection(name).get().then((res) => {
-                //Manage Sub collections
-                let subCollectionNodes = {};
-                let childrenIDs = [];
-                //Iterate Sub Collections
-                subCollections && subCollections.forEach(({type: subType, name: subName, map = node => node, makeNode}) => {
-                    let singleSubCollectionNode = []
-                    res.forEach(doc => {
-
-                        db.collection(`${name}/${doc.id}/${subName}`).get().then(subRes => {
-                            subRes.forEach(subDoc => {
-                                childrenIDs.push(subDoc.id);
-                                const subNodeMeta = {
-                                    id: subDoc.id,
-                                    parent: doc.id,
-                                    children: [],
-                                    internal: {
-                                        type: subType,
-                                        mediaType: `text/html`,
-                                        content: JSON.stringify(subDoc.data()),
-                                        contentDigest: createContentDigest(subDoc.data()),
-                                    },
-                                }
-                                const subNoteData = map ? map(subDoc.data()) : subDoc.data();
-                                const subNode = Object.assign({}, subNoteData, subNodeMeta);
-                                if (makeNode) {
-                                    createNode(subNode);
-                                } else {
-                                    singleSubCollectionNode.push(subNode);
-                                }
-                            });
-                        }).catch(e => console.log(e))
-                        subCollectionNodes = {...subCollectionNodes, [subName]: singleSubCollectionNode}
-                    });
-
-                })
-
-                //Make Collection Node
-                res.forEach(doc => {
-
-                    const nodeMeta = {
-                        id: doc.id,
-                        parent: null,
-                        children: childrenIDs,
+    try {
+        if (firebase.apps || !firebase.apps.length) {
+            const cfg = {credential: firebase.credential.cert(credential)}
+            firebase.initializeApp(cfg);
+        }
+    } catch (e) {
+        console.log(
+            'Could not initialize Firebase. Please check `credential` property in gatsby-config.js'
+        );
+        console.log(e);
+        return;
+    }
+    const db = firebase.firestore();
+    const promises = collections.map(async ({type, name, map = node => node, subCollections}) => {
+        //Get Collection
+        const snapshot = await db.collection(name).get();
+        let subCollectionNodes = {};
+        let childrenIDs = [];
+        for (let doc of snapshot.docs) {
+            subCollections && subCollections.map(async ({type: subType, name: subName, map = node => node}) =>{
+                const subSnapshot = await db.collection(`${name}/${doc.id}/${subName}`).get();
+                for (let subDoc of subSnapshot.docs) {
+                    childrenIDs.push(subDoc.id);
+                    const subNodeMeta = {
+                        id: subDoc.id,
+                        parent: doc.id,
+                        children: [],
                         internal: {
-                            type: type,
+                            type: subType,
                             mediaType: `text/html`,
-                            content: JSON.stringify(doc.data()),
-                            contentDigest: createContentDigest(doc.data()),
+                            content: JSON.stringify(subDoc.data()),
+                            contentDigest: createContentDigest(subDoc.data()),
                         },
                     }
-                    const nodeData = map ? map(doc.data()) : doc.data()
-                    const node = Object.assign({}, {...nodeData, ...subCollectionNodes}, nodeMeta)
-                    createNode(node)
-
-                })
-            }).catch(e => console.log(e));
-        })
-    }).catch((error) => console.log("Firebase Error ", error));
-
-    auth.signOut().catch((e) => console.log(e))
+                    const subNoteData = map ? map(subDoc.data()) : subDoc.data();
+                    const subNode = Object.assign({}, subNoteData, subNodeMeta);
+                    createNode(subNode);
+                }
+            })
+            const nodeMeta = {
+                id: doc.id,
+                parent: null,
+                children: childrenIDs,
+                internal: {
+                    type: type,
+                    mediaType: `text/html`,
+                    content: JSON.stringify(doc.data()),
+                    contentDigest: createContentDigest(doc.data()),
+                },
+            }
+            const nodeData = map ? map(doc.data()) : doc.data()
+            const node = Object.assign({}, {...nodeData, ...subCollectionNodes}, nodeMeta)
+            createNode(node);
+            await Promise.resolve();
+        }
+    })
+    await Promise.all(promises);
 }
+
+
